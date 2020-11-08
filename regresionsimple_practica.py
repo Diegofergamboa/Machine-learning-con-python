0c1d5a7c5b6cf1cb66a53a01281c3f66a2d18293
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-11-07 22:34:14 -0500

    - Belsley, Kuh & Welsch, 'Regression diagnostics: Identifying Influential Data and Sources of Collinearity', Wiley, 1980. 244-261.    - Quinlan,R. (1993). Combining Instance-Based and Model-Based Learning. In Proceedings on the Tenth International Conference of Machine Learning, 236-243, University of Massachusetts, Amherst. Morgan Kaufmann.

--- regresionsimple_practica.py
+++ regresionsimple_practica.py
@@ -1,2 +1,30 @@
 # -*- coding: utf-8 -*-
 
+'''
+    Regresion lineal practica para predecir el valor de las casas en Boston
+'''
+
+import numpy as np
+from sklearn import datasets, linear_model
+import matplotlib as plt
+
+########## PREPARAR LA DATA #########
+
+#Importamos los datos de la misma libreria Scikit - learn
+boston = datasets.load_boston()
+print(boston)
+print()
+
+########## ENTENDIMIENTO DE LA DATA ########
+
+#Verificacion de la informacion contenida en el dataset
+
+print('Informacion de los datos')
+print('')
+print(boston.keys())
+print()
+
+##### CARACTERISTICAS DEL DATASET #######
+
+print('Caracteristicas del Dataset')
+print(boston.DESCR)
--- regresionsimple_teoria.py
+++ regresionsimple_teoria.py
@@ -1,2 +1,32 @@
 # -*- coding: utf-8 -*-
+'''
+    Practica de Regresion Lineal
+'''
+'''
+DEFINIR ALGORITMO =
+linear_model.LinearRegression()
+ESTRENAR MODELO =
+fit(x,y)
+PREDECIR MODELO =
+predict(x)
+CONOCER PENDIENTE (A)
+coef_
+CONOCER INTERCEPTO (B)
+intercept()
+PRESICION DEL MODELO
+score(x)`
+
+#MInimizar la distancia vertical y la linea que seria el modelo
+#Se va a usar MCO LinearRegression con Scikit Learn
+
+# y = ax1 + ax2 + ax3 + b
+
+#Para conocer el valor de la pendiente a, la formula es: a= algoritmo.coef_
+#Para la interseccion se usa la formula que es b = algoritmo.intercept_
+
+#EL rendimiento del algoritmo se evalua directamente despues de realizar
+#la estimacion, de lo contrario arrojarà un error el modelo
+#Evaluar el rendimiento del logaritmo 
+#Usando el R al cuadrado
+# precision = algoritmo.score(x_prueba, y_prueba)
 
